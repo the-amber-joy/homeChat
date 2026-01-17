7512835c7daa577255235ca47dc32f853c5cfd88
Amber Joy <amber.joy.dev@gmail.com>
2026-01-17 17:41:32 -0600

    add browser client and more features

--- client.js
+++ client.js
@@ -2,16 +2,35 @@ var readline = require("readline"),
   socketio = require("socket.io-client"),
   color = require("ansi-color").set;
 var nick;
-var socket = socketio("http://localhost:3636");
+var currentUserList = [];
+var socket = socketio("http://localhost:3010");
 var rl = readline.createInterface(process.stdin, process.stdout);
 
 // Set the username
 rl.question("Please enter a nickname: ", function (name) {
   nick = name;
+  socket.emit("register", nick);
   var msg = nick + " has joined the chat";
   socket.emit("send", { type: "notice", message: msg });
   console_out(color("Welcome to the chat!", "yellow"));
-  show_help();
+  console_out(color("Type /help to see available commands.", "yellow"));
+  console_out("");
+  // Show user list after a short delay to ensure server has sent the list
+  setTimeout(function () {
+    if (currentUserList.length > 0) {
+      console_out(
+        color("Online users (" + currentUserList.length + "):", "yellow"),
+      );
+      currentUserList.forEach(function (user) {
+        var displayUser = "  " + user;
+        if (user === nick) {
+          displayUser += " (you)";
+        }
+        console_out(getUserColor(user) + displayUser + resetColor());
+      });
+      console_out("");
+    }
+  }, 200);
   rl.prompt(true);
 });
 
@@ -22,13 +41,61 @@ function console_out(msg) {
   rl.prompt(true);
 }
 
+function getUserColor(username) {
+  // Generate a consistent color for each username (same algorithm as browser)
+  var hash = 0;
+  for (var i = 0; i < username.length; i++) {
+    hash = username.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  var hue = 40 + (Math.abs(hash) % 320);
+  var saturation = 65 + (Math.abs(hash) % 20);
+  var lightness = 60 + (Math.abs(hash >> 8) % 15);
+
+  // Convert HSL to RGB for ANSI 256 color
+  var h = hue / 360;
+  var s = saturation / 100;
+  var l = lightness / 100;
+
+  var r, g, b;
+  if (s == 0) {
+    r = g = b = l;
+  } else {
+    var hue2rgb = function (p, q, t) {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1 / 6) return p + (q - p) * 6 * t;
+      if (t < 1 / 2) return q;
+      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+      return p;
+    };
+    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    var p = 2 * l - q;
+    r = hue2rgb(p, q, h + 1 / 3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1 / 3);
+  }
+
+  r = Math.round(r * 255);
+  g = Math.round(g * 255);
+  b = Math.round(b * 255);
+
+  // Return ANSI 256 color code
+  return "\x1b[38;2;" + r + ";" + g + ";" + b + "m";
+}
+
+function resetColor() {
+  return "\x1b[0m";
+}
+
 function show_help() {
   console_out(color("Available commands:", "yellow"));
   console_out(color("  /nick <name> - Change your nickname", "yellow"));
   console_out(
-    color("  /msg <user> <message> - Send a private message", "yellow"),
+    color("  @username <message> - Send a private message", "yellow"),
   );
   console_out(color("  /me <action> - Send an emote", "yellow"));
+  console_out(color("  /who - Show online users", "yellow"));
   console_out(color("  /help - Show this help message", "yellow"));
   console_out(color("  /exit - Log out of chat", "yellow"));
 }
@@ -38,6 +105,23 @@ rl.on("line", function (line) {
     var cmd = line.match(/[a-z]+\b/)[0];
     var arg = line.substr(cmd.length + 2, line.length);
     chat_command(cmd, arg);
+  } else if (line[0] == "@" && line.length > 1) {
+    // Handle @username private message
+    var spaceIndex = line.indexOf(" ");
+    if (spaceIndex > 1) {
+      var to = line.substring(1, spaceIndex);
+      var message = line.substring(spaceIndex + 1);
+      socket.emit("send", {
+        type: "tell",
+        message: message,
+        to: to,
+        from: nick,
+      });
+      console_out(color("[" + nick + " -> " + to + "] " + message, "red"));
+    } else {
+      console_out("Usage: @username message");
+    }
+    rl.prompt(true);
   } else {
     // send chat message
     socket.emit("send", { type: "chat", message: line, nick: nick });
@@ -50,17 +134,8 @@ function chat_command(cmd, arg) {
     case "nick":
       var notice = nick + " changed their name to " + arg;
       nick = arg;
+      socket.emit("changeNick", nick);
       socket.emit("send", { type: "notice", message: notice });
-      break;
-    case "msg":
-      var to = arg.match(/[a-z]+\b/)[0];
-      var message = arg.substr(to.length, arg.length);
-      socket.emit("send", {
-        type: "tell",
-        message: message,
-        to: to,
-        from: nick,
-      });
       break;
     case "me":
       var emote = nick + " " + arg;
@@ -68,6 +143,22 @@ function chat_command(cmd, arg) {
       break;
     case "help":
       show_help();
+      break;
+    case "who":
+      if (currentUserList.length === 0) {
+        console_out("No users online.");
+      } else {
+        console_out(
+          color("Online users (" + currentUserList.length + "):", "yellow"),
+        );
+        currentUserList.forEach(function (user) {
+          var displayUser = "  " + user;
+          if (user === nick) {
+            displayUser += " (you)";
+          }
+          console_out(getUserColor(user) + displayUser + resetColor());
+        });
+      }
       break;
     case "exit":
       var notice = nick + " has left the chat";
@@ -84,7 +175,8 @@ function chat_command(cmd, arg) {
 socket.on("message", function (data) {
   var leader;
   if (data.type == "chat" && data.nick != nick) {
-    leader = color("<" + data.nick + "> ", "green");
+    leader =
+      getUserColor(data.nick) + "<" + data.nick + ">" + resetColor() + " ";
     console_out(leader + data.message);
   } else if (data.type == "notice") {
     console_out(color(data.message, "cyan"));
@@ -94,4 +186,8 @@ socket.on("message", function (data) {
   } else if (data.type == "emote") {
     console_out(color(data.message, "cyan"));
   }
+});
+
+socket.on("userList", function (users) {
+  currentUserList = users;
 });
--- server.js
+++ server.js
@@ -1,18 +1,72 @@
 var socketio = require("socket.io");
 var http = require("http");
+var fs = require("fs");
+var path = require("path");
 
 // Create HTTP server and Socket.IO instance
-var server = http.createServer();
+var server = http.createServer(function (req, res) {
+  if (req.url === "/" || req.url === "/index.html") {
+    fs.readFile(path.join(__dirname, "index.html"), function (err, data) {
+      if (err) {
+        res.writeHead(500);
+        res.end("Error loading index.html");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404);
+    res.end("Not found");
+  }
+});
 var io = socketio(server);
 
-// Listen on port 3636
-server.listen(3636);
+// Track connected users
+var users = {};
+
+// App version - increment this when you want clients to reload
+var appVersion = "1.0.0";
+
+// Listen on port 3010 on all network interfaces
+server.listen(3010, "0.0.0.0", function () {
+  console.log("Server listening on port 3010");
+});
 
 io.on("connection", function (socket) {
   var clientIp = socket.handshake.address;
   console.log("A user connected from: " + clientIp);
+
+  // Send app version to client
+  socket.emit("version", appVersion);
+
+  // Handle user registration
+  socket.on("register", function (nickname) {
+    users[socket.id] = nickname;
+    io.emit("userList", Object.values(users));
+  });
+
+  // Handle nickname changes
+  socket.on("changeNick", function (newNick) {
+    users[socket.id] = newNick;
+    io.emit("userList", Object.values(users));
+  });
+
   // Broadcast a user's message to everyone else in the room
   socket.on("send", function (data) {
     io.emit("message", data);
+  });
+
+  // Handle disconnection
+  socket.on("disconnect", function () {
+    if (users[socket.id]) {
+      var nick = users[socket.id];
+      delete users[socket.id];
+      io.emit("userList", Object.values(users));
+      io.emit("message", {
+        type: "notice",
+        message: nick + " has disconnected",
+      });
+    }
   });
 });
